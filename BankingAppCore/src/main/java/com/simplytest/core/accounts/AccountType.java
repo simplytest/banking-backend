004b4fa401502515510f74e9af61b1917769e727
Curve <fynnbwdt@gmail.com>
2023-11-22 13:44:09 +0100

    refactor(AccountType): remove `getType`

--- BankingAppCore/src/main/java/com/simplytest/core/accounts/AccountType.java
+++ BankingAppCore/src/main/java/com/simplytest/core/accounts/AccountType.java
@@ -3,21 +3,4 @@ package com.simplytest.core.accounts;
 public enum AccountType
 {
     GiroAccount, FixedRateAccount, OnCallAccount, RealEstateAccount;
-
-    public static AccountType getType(String type)
-    {
-        switch (type)
-        {
-            case "Giro Konto":
-                return AccountType.GiroAccount;
-            case "Tagesgeld Konto":
-                return AccountType.OnCallAccount;
-            case "Festgeld Konto":
-                return AccountType.FixedRateAccount;
-            case "Immobilienkredit Konto":
-                return AccountType.RealEstateAccount;
-        }
-
-        throw new UnsupportedOperationException();
-    }
 }
