1999dcdfc0a751ec0c39f9c36f4690c53367c75d
Curve <fynnbwdt@gmail.com>
2023-12-10 16:20:41 +0100

    feat: update demo users

--- BankingAppServer/src/main/java/com/simplytest/server/BankingServer.java
+++ BankingAppServer/src/main/java/com/simplytest/server/BankingServer.java
@@ -9,6 +9,7 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.mock.web.MockHttpServletResponse;
 
+import com.simplytest.core.accounts.AccountType;
 import com.simplytest.core.customers.Address;
 import com.simplytest.server.api.ContractController;
 import com.simplytest.server.apiData.CustomerData;
@@ -20,6 +21,33 @@ public class BankingServer
     @Autowired
     private ContractController controller;
 
+    public static CustomerData createDemoUser(String name, String password)
+    {
+        var address = new Address();
+
+        address.setCity("City");
+        address.setCountry("Country");
+        address.setEmail("email@demo.com");
+        address.setHouse("House");
+        address.setStreet("Street");
+        address.setZipCode("12345");
+
+        var birthDay = Calendar.getInstance();
+        birthDay.set(2000, 01, 01);
+
+        var rtn = new CustomerData();
+
+        rtn.address = address;
+        rtn.type = CustomerType.Private;
+        rtn.birthDay = birthDay.getTime();
+
+        rtn.lastName = name;
+        rtn.firstName = name;
+        rtn.password = password;
+
+        return rtn;
+    }
+
     @EventListener(ApplicationReadyEvent.class)
     public void initializeDemo()
     {
@@ -28,29 +56,16 @@ public class BankingServer
             return;
         }
 
-        var data = new CustomerData();
-        data.address = new Address();
-
-        data.address.setCity("City");
-        data.address.setCountry("Country");
-        data.address.setEmail("email@demo.com");
-        data.address.setHouse("House");
-        data.address.setStreet("Street");
-        data.address.setZipCode("12345");
-
-        var birthDay = Calendar.getInstance();
-        birthDay.set(2000, 01, 01);
-
-        data.lastName = "Demo";
-        data.firstName = "Demo";
-
-        data.type = CustomerType.Private;
-        data.birthDay = birthDay.getTime();
-
-        data.password = "demo";
-
         var response = new MockHttpServletResponse();
-        controller.registerContract(data, 1000.0, response);
+
+        controller.registerContract(createDemoUser("demo", "demo"), 1000.0,
+                response);
+
+        var john = createDemoUser("John", "123");
+        var result = controller.registerContract(john, 0.0, response);
+        controller.addAccount(result.value().JWT(), AccountType.FixedRateAccount);
+
+        controller.registerContract(createDemoUser("Amanda", "123"), 0.0, response);
     }
 
     public static void main(String[] args)
