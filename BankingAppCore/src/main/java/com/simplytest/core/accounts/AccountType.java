9f67dafde81ccf7a6943492e0cbbaf8a3bb0ae14
axheim <34610354+axheim@users.noreply.github.com>
2024-01-04 11:40:11 +0100

    merge: refac: tests simplified and stabilized

--- BankingAppCore/src/main/java/com/simplytest/core/accounts/AccountType.java
+++ BankingAppCore/src/main/java/com/simplytest/core/accounts/AccountType.java
@@ -1,6 +1,29 @@
 package com.simplytest.core.accounts;
 
+import com.simplytest.core.Error;
+
 public enum AccountType
 {
-    GiroAccount, FixedRateAccount, OnCallAccount, RealEstateAccount;
+    GiroAccount,
+    FixedRateAccount,
+    OnCallAccount,
+    RealEstateAccount;
+
+    public static AccountType getType(String type)
+    {
+        switch (type)
+        {
+            case "Giro Konto":
+                return AccountType.GiroAccount;
+            case "Tagesgeld Konto":
+                return AccountType.OnCallAccount;
+            case "Festgeld Konto":
+                return AccountType.FixedRateAccount;
+            case "Immobilien-Finanzierungskonto":
+                return AccountType.RealEstateAccount;
+        }
+
+        throw new UnsupportedOperationException();
+    }
+
 }
