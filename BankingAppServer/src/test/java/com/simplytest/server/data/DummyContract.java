41b20f54b274117386714311a22dca4fa33ab52a
Philippe Joseph <p.joseph@simplytest.de>
2025-02-06 15:21:21 +0100

    add ability to add an realEstate Acoount to contract

--- BankingAppServer/src/test/java/com/simplytest/server/data/DummyContract.java
+++ BankingAppServer/src/test/java/com/simplytest/server/data/DummyContract.java
@@ -1,5 +1,117 @@
 package com.simplytest.server.data;
 
+import com.simplytest.core.contracts.Contract;
+import com.simplytest.server.json.Json;
+
 public class DummyContract {
     // create dummy contracts with different account settings
+    public Contract createDummyContract() {
+        String dummy = """
+                {
+                  "id": {
+                    "counter": 1,
+                    "parent": 1,
+                    "child": 0
+                  },
+                  "customer": {
+                    "type": "com.simplytest.core.customers.CustomerPrivate",
+                    "data": {
+                      "birthDay": "1985-04-18",
+                      "schufaScore": 0.0,
+                      "transactionFee": 0.0,
+                      "monthlyFee": 2.99,
+                      "firstName": "Foo",
+                      "lastName": "Bar",
+                      "address": {
+                        "country": "Deutschland",
+                        "zipCode": "12345",
+                        "street": "Some Street"
+                      }
+                    }
+                  },
+                  "passwordHash": "$2a$12$YkHRZthCXrh/OoXy7QG1suKccQ6pkIh8UlStfWPpbouqOVD0UcEyK",
+                  "accounts": [
+                    [
+                      {
+                        "counter": 1,
+                        "parent": 1,
+                        "child": 1
+                      },
+                      {
+                        "type": "com.simplytest.core.accounts.AccountGiro",
+                        "data": {
+                          "sendLimit": 3000.0,
+                          "dispoLimit": 0.0,
+                          "dispoRate": 0.0,
+                          "balance": 0.0,
+                          "boundPeriod": 0.0,
+                          "interestRate": 0.0
+                        }
+                      }
+                    ]
+                  ]
+                }
+                """;
+        return Json.get().fromJson(dummy, Contract.class);
+    }
+
+    public Contract createDummyWithRE() {
+        var dummy = """
+                {
+                  "id": {
+                    "counter": 2,
+                    "parent": 1,
+                    "child": 0
+                  },
+                  "customer": {
+                    "type": "com.simplytest.core.customers.CustomerPrivate",
+                    "data": {
+                      "birthDay": "Apr 18, 1985, 12:00:00 AM",
+                      "schufaScore": 0.0,
+                      "transactionFee": 0.0,
+                      "monthlyFee": 2.99,
+                      "firstName": "Foo",
+                      "lastName": "Bar",
+                      "address": {
+                        "country": "Deutschland",
+                        "zipCode": "12345",
+                        "street": "Some Street"
+                      }
+                    }
+                  },
+                  "accounts": {
+                    "00001:00001": {
+                      "type": "com.simplytest.core.accounts.AccountGiro",
+                      "data": {
+                        "sendLimit": 3000.0,
+                        "dispoLimit": 0.0,
+                        "dispoRate": 0.0,
+                        "balance": 0.0,
+                        "boundPeriod": 0.0,
+                        "interestRate": 0.0
+                      }
+                    },
+                    "00001:00002": {
+                      "type": "com.simplytest.core.accounts.AccountRealEstate",
+                      "data": {
+                        "creditAmount": 35000.0,
+                        "remainingAmount": 0.0,
+                        "payedAmount": 0.0,
+                        "runtimeAmount": 0.0,
+                        "repaymentRate": 500.0,
+                        "monthlyAmount": 1458333.3333333333,
+                        "balance": -35000.0,
+                        "boundPeriod": 0.0,
+                        "interestRate": 0.0
+                      }
+                    }
+                  }
+                }""";
+        return Json.get().fromJson(dummy, Contract.class);
+    }
+
+    public Contract addRealEstateToDummy(Contract dummy, double creditAmount, double repaimentRate) {
+        dummy.openRealEstateAccount(repaimentRate, creditAmount);
+        return dummy;
+    }
 }
--- BankingAppServer/src/test/java/com/simplytest/server/integration/ContractControllerTestU3.java
+++ BankingAppServer/src/test/java/com/simplytest/server/integration/ContractControllerTestU3.java
@@ -1,17 +1,27 @@
 package com.simplytest.server.integration;
 
+import com.google.common.reflect.TypeToken;
+import com.simplytest.core.Error;
+import com.simplytest.core.Id;
+import com.simplytest.core.accounts.IAccount;
 import com.simplytest.core.contracts.Contract;
+import com.simplytest.core.utils.Pair;
+import com.simplytest.server.apiData.ContractRegistrationResult;
 import com.simplytest.server.apiData.CustomerData;
 import com.simplytest.server.auth.JWT;
+import com.simplytest.server.data.DummyContract;
 import com.simplytest.server.json.Json;
 import com.simplytest.server.model.DBContract;
 import com.simplytest.server.repo.ContractRepository;
+import com.simplytest.server.utils.Result;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+
+import java.lang.reflect.Type;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ContractControllerTestU3 {
@@ -25,15 +35,15 @@ public class ContractControllerTestU3 {
     @Test
     public void getContractInformation() {
         String url = "/api/contracts";
-        var dbContract = repository.save(new DBContract(createDummyContract()));
-//        var customerContract = registerNewCustomer();
-//
+        var dummyContract = new DummyContract();
+        var dbContract = repository.save(new DBContract(dummyContract.createDummyContract()));
+
         HttpHeaders headers = new HttpHeaders();
         headers.set(HttpHeaders.AUTHORIZATION, JWT.generate(dbContract.id()));
         headers.setContentType(MediaType.APPLICATION_JSON);
-//
+
         HttpEntity<String> entity = new HttpEntity<>(headers);
-//
+
         ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
         Assertions.assertAll(
                 () -> Assertions.assertEquals(HttpStatus.OK, response.getStatusCode()),
@@ -42,84 +52,83 @@ public class ContractControllerTestU3 {
         Contract contract = Json.get().fromJson(response.getBody(), Contract.class);
         System.out.println(response.getBody());
         Assertions.assertAll(
-                () -> Assertions.assertEquals("hanibal", contract.getCustomer().getFirstName()),
-                () -> Assertions.assertEquals("lecter", contract.getCustomer().getLastName()),
-                () -> Assertions.assertEquals("Mr Hyde Str", contract.getCustomer().getAddress().getStreet()),
-                () -> Assertions.assertEquals("Gotham", contract.getCustomer().getAddress().getCity()),
-                () -> Assertions.assertEquals("Germany", contract.getCustomer().getAddress().getCountry()),
-                () ->Assertions.assertEquals("878765", contract.getCustomer().getAddress().getZipCode()),
-                () ->Assertions.assertEquals("42", contract.getCustomer().getAddress().getHouse()),
-                () ->Assertions.assertEquals("mey", contract.getCustomer().getAddress().getEmail())
+                () -> Assertions.assertEquals("Foo", contract.getCustomer().getFirstName()),
+                () -> Assertions.assertEquals("Bar", contract.getCustomer().getLastName())
 
         );
     }
-    private CustomerData createCustomer() {
-        String customer = "{\n" + //
-                "  \"firstName\": \"hanibal\",\n" + //
-                "  \"password\": \"demo\",\n" + //
-                "  \"lastName\": \"lecter\",\n" + //
-                "  \"address\": {\n" + //
-                "    \"country\": \"Germany\",\n" + //
-                "    \"zipCode\": \"878765\",\n" + //
-                "    \"street\": \"Mr Hyde Str\",\n" + //
-                "    \"house\": \"42\",\n" + //
-                "    \"city\": \"Gotham\",\n" + //
-                "    \"email\": \"mey\"\n" + //
-                "  },\n" + //
-                "  \"type\": \"Private\",\n" + //
-                "  \"birthDay\": \"1984-06-27T14:12:49.117Z\",\n" + //
-                "  \"ustNumber\": \"35252342\",\n" + //
-                "  \"companyName\": \"Imortal Inc.\"\n" + //
-                "}";
-        return Json.get().fromJson(customer, CustomerData.class);
-    }
-    private Contract createDummyContract() {
-        String dummy = """
+
+    @Test
+    public void addAccountToDBCreatedUser() {
+        String url = "/api/contracts/accounts";
+        var dummyContract = new DummyContract();
+        var dbContract = repository.save(new DBContract(dummyContract.createDummyContract()));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, JWT.generate(dbContract.id()));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        String realEstate = """
                 {
-                  "id": {
-                    "counter": 1,
-                    "parent": 1,
-                    "child": 0
-                  },
-                  "customer": {
-                    "type": "com.simplytest.core.customers.CustomerPrivate",
-                    "data": {
-                      "birthDay": "1985-04-18",
-                      "schufaScore": 0.0,
-                      "transactionFee": 0.0,
-                      "monthlyFee": 2.99,
-                      "firstName": "Foo",
-                      "lastName": "Bar",
-                      "address": {
-                        "country": "Deutschland",
-                        "zipCode": "12345",
-                        "street": "Some Street"
-                      }
-                    }
-                  },
-                  "passwordHash": "$2a$12$YkHRZthCXrh/OoXy7QG1suKccQ6pkIh8UlStfWPpbouqOVD0UcEyK",
-                  "accounts": [
-                    [
-                      {
-                        "counter": 1,
-                        "parent": 1,
-                        "child": 1
-                      },
-                      {
-                        "type": "com.simplytest.core.accounts.AccountGiro",
-                        "data": {
-                          "sendLimit": 3000.0,
-                          "dispoLimit": 0.0,
-                          "dispoRate": 0.0,
-                          "balance": 0.0,
-                          "boundPeriod": 0.0,
-                          "interestRate": 0.0
-                        }
-                      }
-                    ]
-                  ]
-                }
-                """;
-        return Json.get().fromJson(dummy, Contract.class);
+                  "repaymentRate": 500,
+                  "amount": 35000
+                }""";
+
+        HttpEntity<String> entity = new HttpEntity<>(realEstate, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+        // Pair<Id, IAccount>
+        Type account = new TypeToken<Pair<Id, IAccount>>() {
+        }.getType();
+        final var parsed = (Pair<Id, IAccount>) Json.get().fromJson(response.getBody(), account);
+
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(HttpStatus.OK, response.getStatusCode()),
+                () -> Assertions.assertTrue(parsed.second().getType().toString().contains("RealEstate")),
+                () -> Assertions.assertEquals(-35000, parsed.second().getBalance()),
+                () -> Assertions.assertEquals(0, parsed.second().getInterestRate())
+        );
+
+        headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, JWT.generate(dbContract.id()));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> contractResponse = restTemplate.exchange("/api/contracts", HttpMethod.GET, entity, String.class);
+        Assertions.assertEquals(HttpStatus.OK, contractResponse.getStatusCode());
+
+        Contract contract = Json.get().fromJson(contractResponse.getBody(), Contract.class);
+        System.out.println(contractResponse.getBody());
+        Assertions.assertAll(
+                () -> Assertions.assertEquals("Foo", contract.getCustomer().getFirstName()),
+                () -> Assertions.assertEquals("Bar", contract.getCustomer().getLastName()),
+                () -> Assertions.assertTrue(contractResponse.getBody().contains("AccountGiro")),
+                () -> Assertions.assertTrue(contractResponse.getBody().contains("AccountRealEstate"))
+        );
+    }
+
+    @Test
+    public void testUserWithModifiedAccount() {
+        var dummyContract = new DummyContract();
+        var contract = dummyContract.createDummyContract();
+        contract = dummyContract.addRealEstateToDummy(contract, 35000, 500);
+        var dbContract = repository.save(new DBContract(contract));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, JWT.generate(dbContract.id()));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<String> contractResponse = restTemplate.exchange("/api/contracts", HttpMethod.GET, entity, String.class);
+        Assertions.assertEquals(HttpStatus.OK, contractResponse.getStatusCode());
+
+        Contract newContract = Json.get().fromJson(contractResponse.getBody(), Contract.class);
+        System.out.println(contractResponse.getBody());
+        Assertions.assertAll(
+                () -> Assertions.assertEquals("Foo", newContract.getCustomer().getFirstName()),
+                () -> Assertions.assertEquals("Bar", newContract.getCustomer().getLastName()),
+                () -> Assertions.assertTrue(contractResponse.getBody().contains("AccountGiro")),
+                () -> Assertions.assertTrue(contractResponse.getBody().contains("AccountRealEstate"))
+        );
     }
 }
